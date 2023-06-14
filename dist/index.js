fcb985a7c0971b903e5409288235d42a73fb7040
Mr-Hera <randellhera@gmail.com>
2023-06-14 18:23:25 +0300

    Feat: successfully explored Typescript with both js & react

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,73 @@
 "use strict";
+// Basic types
 let id = 5;
-console.log('ID: ', id);
+let company = 'Randell Hera';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5, 6];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Mr-Hera', true];
+let employee;
+employee = [
+    [1, 'Randell'],
+    [2, 'Hera']
+];
+let pid = 22;
+pid = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'Hera'
+};
+// Type Assertion
+let cid = 1;
+// let customerId = cid as string
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'Unknown'
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} has been registered successfully!`;
+    }
+}
+const brad = new Person(1, 'Brad');
+const mike = new Person(2, 'Mike');
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+const emp = new Employee(3, 'Shawn', 'Developer');
+console.log(emp.register());
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,125 @@
 // Basic types
 let id: number = 5
+let company: string = 'Randell Hera'
+let isPublished = true
+let x: any = 'Hello'
 
+let ids: number[] = [1, 2, 3, 4, 5, 6]
+let arr: any[] = [1, true, 'Hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Mr-Hera', true]
+let employee: [number, string][]
+
+employee = [
+    [1, 'Randell'],
+    [2, 'Hera']
+]
+
+let pid: number | string = 22
+pid = '22'
+
+// Enum
+enum Direction1 {
+    Up=1,
+    Down,
+    Left,
+    Right
+}
+
+enum Direction2 {
+    Up='Up',
+    Down='Down',
+    Left='Left',
+    Right='Right'
+}
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'Hera'
+}
+
+// Type Assertion
+let cid: any = 1
+// let customerId = cid as string
+let customerId = <string>cid
+
+// Functions
+function addNum(x:number, y:number):number {
+    return x + y
+}
+
+// Void
+function log(message: string | number): void {
+    console.log(message)
+}
+
+// Interfacse
+interface UserInterface {
+    readonly id: number,
+    name: string,
+    age?: number
+}
+
+const user1: UserInterface = {
+    id: 1,
+    name: 'Unknown'
+}
+
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
+
+// Classes
+interface PersonInterface {
+    id: number
+    name: string
+    register(): string
+}
+class Person implements PersonInterface {
+    id: number
+    name: string
+
+    constructor(id: number, name: string) {
+        this.id = id
+        this.name = name
+    }
+
+    register() {
+        return `${this.name} has been registered successfully!`
+    }
+}
+
+const brad = new Person(1, 'Brad')
+const mike = new Person(2, 'Mike')
+
+// Subclasses
+class Employee extends Person {
+    position: string
+
+    constructor (id: number, name: string, position: string) {
+        super(id, name)
+        this.position = position
+    }
+}
+
+const emp = new Employee(3, 'Shawn', 'Developer')
+
+// Generics
+function getArray<T>(items: T[]): T[] {
+    return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1, 2, 3, 4])
+let strArray = getArray<string>(['Brad', 'John', 'Jill'])
+
+strArray.push('Hello')
